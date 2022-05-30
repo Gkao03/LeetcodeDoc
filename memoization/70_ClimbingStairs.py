4ec14554afda7ab8782139a75c5a3cf13aa7e7b5
Gkao03 <gorek@andrew.cmu.edu>
2022-05-30 08:43:01 -0400

    climbing stairs

---
+++ memoization/70_ClimbingStairs.py
@@ -0,0 +1,27 @@
+# Climbing Stairs
+# Time: O(n). Pseudo linear where n is the numeric value of the input
+# Space: O(1)
+# Topics: Math, Dynamic Programming, Memoization
+# Difficulty: Easy
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # n is at least 3
+        prev_2steps = 1
+        prev_1step = 2
+        
+        step_num = 3
+        
+        while step_num <= n:
+            curr_ways = prev_2steps + prev_1step
+            prev_2steps = prev_1step
+            prev_1step = curr_ways
+            
+            step_num += 1
+            
+        return curr_ways

c35aa7d8c233f290759cda4376f94654fce36c16
Gkao03 <gorek@andrew.cmu.edu>
2022-06-05 15:36:18 -0400

    binary search

---
+++ array/704_BinarySearch.py
@@ -0,0 +1,24 @@
+# Binary Search
+# Time: O(logn)
+# Space: O(1)
+# Topics: Array, Binary Search
+# Difficulty: Easy
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+                
+        return -1

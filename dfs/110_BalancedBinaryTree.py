25356ac75bd5ed7b1c9baa72fa426354836ee1c5
Gkao03 <gorek@andrew.cmu.edu>
2022-06-08 02:14:12 -0400

    balanced binary tree O(n)

--- dfs/110_BalancedBinaryTree.py
+++ dfs/110_BalancedBinaryTree.py
@@ -1,8 +1,9 @@
 # Balanced Binary Tree
 # Time: O(n)
-# Space: O(n). stack space
+# Space: O(h) stack space where h is height of tree. O(n) if not balanced. O(logn) if balanced.
 # Topics: Tree, Depth-First Search, Binary Tree
 # Difficulty: Easy
+# Notes: to get from O(n^2) to O(n) time, calculate height and balance in same dfs to avoid repeated computation (see helper)
 
 from typing import Optional
 
@@ -14,32 +15,23 @@ class TreeNode:
         self.right = right
 
 
-class Height:
-    def __init__(self):
-        self.height = 0
-
-        
 class Solution:
-    def height(self, root):
-        if root is None:
-            return 0
-        return max(self.height(root.left), self.height(root.right)) + 1
+    def helper(self, node):  # return tuple (T/F, height)
+        if node is None:
+            return True, 0
+        
+        left = self.helper(node.left)
+        right = self.helper(node.right)
+        
+        left_is_balanced, left_height = left[0], left[1]
+        right_is_balanced, right_height = right[0], right[1]
+        
+        is_balanced = left_is_balanced and right_is_balanced and abs(left_height - right_height) <= 1
+        
+        return is_balanced, max(left_height, right_height) + 1
         
     
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
+        is_balanced, _ = self.helper(root)
         
-        lh = Height()
-        rh = Height()
-
-        lh.height = self.height(root.left)
-        rh.height = self.height(root.right)
-
-        l = self.isBalanced(root.left)
-        r = self.isBalanced(root.right)
-
-        if abs(lh.height - rh.height) <= 1:
-            return l and r
-        
-        return False
+        return is_balanced

364b88d8b90821e7bac6c8f108981c4ae7eba5e5
Gkao03 <gorek@andrew.cmu.edu>
2022-08-31 19:26:07 -0400

    optimize gas station code

--- array/134_GasStation.py
+++ array/134_GasStation.py
@@ -9,63 +9,19 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        n = len(gas)
-        start_idx = -1
+        total_gas = 0
+        rest_gas = 0
+        start = 0
         
-        for i in range(n):
-            if gas[i] >= cost[i]:
-                start_idx = i
-                break
+        for i, (g, c) in enumerate(zip(gas, cost)):
+            new_gas = g - c
+            
+            total_gas += new_gas
+            rest_gas += new_gas
+            
+            if rest_gas < 0:
+                start = i + 1
+                rest_gas = 0
                 
-        if start_idx == -1:
-            return -1
-        
-        # initialization
-        curr_idx = start_idx
-        answer_idx = start_idx
-        prev_answer_idx = answer_idx
-        past_start = False
-        
-        # do while
-        curr_gas_in_car = gas[start_idx] - cost[start_idx]
-        curr_idx = (curr_idx + 1) % n
-        
-        while answer_idx != curr_idx:
-            if answer_idx == -1 and gas[curr_idx] >= cost[curr_idx]:
-                if start_idx < prev_answer_idx:
-                    if curr_idx > prev_answer_idx or curr_idx < start_idx:
-                        answer_idx = curr_idx
-                        prev_answer_idx = answer_idx
-                        past_start = False
-                    else:
-                        return -1
-                elif start_idx > prev_answer_idx:
-                    if prev_answer_idx < curr_idx < start_idx:
-                        answer_idx = curr_idx
-                        prev_answer_idx = answer_idx
-                        past_start = False
-                    else:
-                        return -1
-                else:  # start_idx == prev_answer_idx
-                    if curr_idx == start_idx:
-                        return -1
-                    answer_idx = curr_idx if prev_answer_idx != answer_idx else -1
-                    if answer_idx == -1:
-                        return -1
-                    prev_answer_idx = answer_idx
-                    past_start = False
-
-            if curr_idx == answer_idx and past_start is True:
-                return answer_idx
-                
-            curr_gas_in_car += gas[curr_idx]
-            if curr_gas_in_car < cost[curr_idx]:  # can't make it to next station
-                answer_idx = -1
-                curr_gas_in_car = 0
-            else:  # can make it to next station
-                curr_gas_in_car = curr_gas_in_car - cost[curr_idx]
-                
-            curr_idx = (curr_idx + 1) % n
-            past_start = True
-        
-        return answer_idx
+        start = -1 if total_gas < 0 else start
+        return start

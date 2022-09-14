e4aa3071c7b1e5b36d185b96bada8eb4573600f3
Gkao03 <gorek@andrew.cmu.edu>
2022-09-14 19:17:44 -0400

    search 2D matrix update

--- matrix/74_Search2DMatrix.py
+++ matrix/74_Search2DMatrix.py
@@ -3,27 +3,26 @@
 # Space: O(1)
 # Topics: Array, Binary Search, Matrix
 # Difficulty: Medium
-# Notes: this implementation uses O(m) space to get the column array,
-# however it can be trivially converted to O(1) space by writing
-# custome binary search based on row indices.
+# Notes: this implementation uses binary search on the matrix as
+# if it was simulated as a flattened array.
 
 from typing import List
-from bisect import bisect_left, bisect_right
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-        col = [matrix[i][0] for i in range(m)]
+        low, high = 0, m * n - 1
         
-        row_idx = bisect_right(col, target)
-        if row_idx == 0:
-            return False
-        
-        col_idx = bisect_left(matrix[row_idx - 1], target)
-        if col_idx == n:
-            return False
-        
-        if matrix[row_idx - 1][col_idx] == target:
-            return True
-        
+        while low <= high:
+            mid = (low + high) // 2
+            mid_i = mid // n
+            mid_j = mid % n
+            
+            if matrix[mid_i][mid_j] == target:
+                return True
+            elif matrix[mid_i][mid_j] < target:  # binary search right side
+                low = mid + 1
+            else:  #  matrix[mid_i][mid_j] > target
+                high = mid - 1
+                
         return False

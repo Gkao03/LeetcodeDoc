00fdc9f415a84ffd30b8e47d238cc9d88a9aafc8
Gkao03 <gorek@andrew.cmu.edu>
2023-01-15 19:59:08 -0500

    update number of good leaf node pairs

--- binary_tree/1530_NumberOfGoodLeafNodesPairs.py
+++ binary_tree/1530_NumberOfGoodLeafNodesPairs.py
@@ -1,6 +1,6 @@
 # Number of Good Leaf Nodes Pairs
 # Time: O(nd^2). n is number of nodes in tree. d is distance.
-# Space: O(nd). n is number f nodes in tree. d is distance.
+# Space: O(nd). n is number of nodes in tree. d is distance.
 # Topics: Tree, Depth-First Search, Binary Tree
 # Difficulty: Medium
 # Notes: while d is an input parameter, since the constraint d << n,
@@ -20,27 +20,27 @@ class TreeNode:
 class Solution:
     def dfs(self, curr_node, hashmap):
         if curr_node.left is None and curr_node.right is None:
-            new_hashmap = deque([0] * (self.distance + 1))
+            new_hashmap = [0] * (self.distance + 1)
             new_hashmap[1] = 1
             return new_hashmap
 
-        left_hashmap = deque([0] * (self.distance + 1))
+        left_hashmap = [0] * (self.distance + 1)
         if curr_node.left is not None:
             new_hashmap = copy.deepcopy(hashmap)
 
             # shift
-            new_hashmap.rotate(1)
+            new_hashmap[1:] = new_hashmap[:-1]
             new_hashmap[0] = 0
 
             # call left
             left_hashmap = self.dfs(curr_node.left, new_hashmap)
 
-        right_hashmap = deque([0] * (self.distance + 1))
+        right_hashmap = [0] * (self.distance + 1)
         if curr_node.right is not None:
             new_hashmap = copy.deepcopy(hashmap)
 
             # shift
-            new_hashmap.rotate(1)
+            new_hashmap[1:] = new_hashmap[:-1]
             new_hashmap[0] = 0
 
             # call right
@@ -48,10 +48,10 @@ class Solution:
 
         # update answer
         for dist in range(1, self.distance):
-            right_sum = sum(list(itertools.islice(right_hashmap, 0, self.distance - dist + 1)))
+            right_sum = sum(right_hashmap[:self.distance - dist + 1])
             self.answer += left_hashmap[dist] * right_sum
 
-        merged_hashmap = deque([0] * (self.distance + 1))
+        merged_hashmap = [0] * (self.distance + 1)
         for dist in range(self.distance, 0, -1):
             merged_hashmap[dist] = left_hashmap[dist - 1] + right_hashmap[dist - 1]
 
@@ -60,6 +60,6 @@ class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
         self.distance = distance
         self.answer = 0
-        self.dfs(root, deque([0] * (distance + 1)))
+        self.dfs(root, [0] * (distance + 1))
 
         return self.answer
